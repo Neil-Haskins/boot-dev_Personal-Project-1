4075df8a5d28ed7729bf1ecd76b899cbaed026b0
Neil Haskins <neil.haskins@studyforge.net>
2024-01-27 13:05:48 -0800

    Implement FilterSet class

--- gui.py
+++ gui.py
@@ -27,47 +27,19 @@ class Window:
         self.__main_frame = ttk.Frame(self.__root, padding=(8,5))
         self.__main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
 
-        self.__month_start_label = ttk.Label(self.__main_frame, text="Start month:")
-        self.__month_start_label.grid(column=1, row=1, sticky=(W))
-
-        self.__month_start_select_options = StringVar()
-        self.__month_start_select_options.set('jan')
-        self.__month_start_select_box = OptionMenu(
-                self.__main_frame,
-                self.__month_start_select_options,
-                *filters.months
-                )
-        self.__month_start_select_box.grid(column=2, row=1, sticky=(W, E))
-
-        # TODO: remove this
-        self.__month_start_select_options.trace('w', lambda *args : change_dropdown(self.__month_start_select_options))
-
-        self.__month_end_label = ttk.Label(self.__main_frame, text="End month:")
-        self.__month_end_label.grid(column=1, row=2, sticky=(W))
-
-        self.__month_end_select_options = StringVar()
-        self.__month_end_select_options.set('jan')
-        self.__month_end_select_box = OptionMenu(self.__main_frame, self.__month_end_select_options, *filters.months)
-        self.__month_end_select_box.grid(column=2, row=2, sticky=(W, E))
-
-        # TODO: remove this
-        self.__month_end_select_options.trace('w', lambda *args : change_dropdown(self.__month_end_select_options))
-
-
-        self.__filter_label = ttk.Label(self.__main_frame, text="Filter by:")
-        self.__filter_label.grid(column=1, row=3, sticky=(W))
-
-        self.__filter_select_options = StringVar()
-        self.__filter_select_options.set('Select filter')
-        self.__filter_select_box = OptionMenu(self.__main_frame, self.__filter_select_options, *filters.filters_dict.keys())
-        self.__filter_select_box.grid(column=2, row=3, sticky=(W, E))
-
+        self.__start_month = FilterSet(self.__main_frame, 1, "Start month:", filters.months)
+        self.__start_month.set('Select month')
+        self.__end_month = FilterSet(self.__main_frame, 2, "End month:", filters.months)
+        self.__end_month.set('Select month')
+        self.__filter_by = FilterSet(self.__main_frame, 3, "Filter by:", filters.filters_dict.keys())
+        self.__filter_by.set('Select filter')
 
         self.__filter_value_label_text = StringVar()
         self.__filter_value_label_text.set("Filter value:")
         self.__filter_value_label = ttk.Label(self.__main_frame, textvariable=self.__filter_value_label_text)
         self.__filter_value_label.grid(column=1, row=4, sticky=(W, E))
-        self.__filter_select_options.trace('w', lambda *args : self.__filter_value_label_text.set(f'{self.__filter_select_options.get()} = '))
+
+        self.__filter_by.trace('w', lambda *args : self.__filter_value_label_text.set(f'{self.__filter_by.get()} = '))
 
         self.__filter_value = StringVar()
         self.__filter_value_entry = ttk.Entry(self.__main_frame, textvariable=self.__filter_value, validate='all', validatecommand=(self.validate_entry, '%P'))
@@ -76,17 +48,19 @@ class Window:
         self.__find_button = ttk.Button(self.__main_frame, text='Find cities', command=self.on_click_find)
         self.__find_button.grid(column=2, row=5)
 
+
+
         for child in self.__main_frame.winfo_children(): 
             child.grid_configure(padx=5, pady=3)
         
         self.__root.mainloop()
 
     def on_click_find(self):
-        filter_name = self.__filter_select_options.get()
+        filter_name = self.__filter_by.get()
         value = float(self.__filter_value.get())
         use_filter = filters.filters_dict[filter_name]
-        start_month = self.__month_start_select_options.get()
-        end_month = self.__month_end_select_options.get()
+        start_month = self.__start_month.get()
+        end_month = self.__end_month.get()
         months_dict = filters.filter_months(self._cities_dict, start_month, end_month)
         result = use_filter(months_dict, value)
         nicely_print(result)
@@ -103,3 +77,26 @@ class Window:
 # Stringvar(s) to enter value(s) for selected filter
 # Get Results button
 # Text display of results
+
+class FilterSet:
+    def __init__(self, parent, row, label_text, options_set):
+        label = ttk.Label(parent, text=label_text)
+        label.grid(column=1, row=row, sticky=(W))
+
+        self._select_options = StringVar()
+        self._select_options.set('')
+        select_box = OptionMenu(
+            parent,
+            self._select_options,
+            *options_set
+            )
+        select_box.grid(column=2, row=row, sticky=(W, E))
+    
+    def get(self):
+        return self._select_options.get()
+    
+    def set(self, *args):
+        return self._select_options.set(*args)
+
+    def trace(self, *args):
+        return self._select_options.trace(*args)
